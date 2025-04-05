464878d2ddf22b910e8415456e4ed3f6d27ee958
koki <shintech9898@gmail.com>
2025-04-05 18:43:24 +0900

    Refactoring the Grouping Program

--- python/grouping.py
+++ python/grouping.py
@@ -1,24 +1,32 @@
 import random
 
 l = ["A", "B", "C", "D", "E", "F"]
+# リファクタリング
+group_pattern = [2, 3, 4]
+chose_pattern = random.choice(group_pattern)
+group1 = random.sample(l, chose_pattern)
+group2 = [person for person in l if not person in group1]
 
-# 完成形
-group1_l = []
-group2_l = []
-group_choice = (group1_l, group2_l)
+print(group1)
+print(group2)
 
-for v in l:
-    if len(group1_l) < 4 and len(group2_l) < 4:
-        random.choice((group1_l,group2_l)).append(v)
-    elif len(group1_l) == 4:
-        group2_l.append(v)
-    elif len(group2_l) == 4:
-        group1_l.append(v)
+# 完成形----------------------------------------------------------------------------------------------------------
+# group1_l = []
+# group2_l = []
+# group_choice = (group1_l, group2_l)
 
-print(group1_l)
-print(group2_l)
+# for v in l:
+#     if len(group1_l) < 4 and len(group2_l) < 4:
+#         random.choice((group1_l,group2_l)).append(v)
+#     elif len(group1_l) == 4:
+#         group2_l.append(v)
+#     elif len(group2_l) == 4:
+#         group1_l.append(v)
 
-# 「実装時に頭真っ白…」でもう悩まない！プログラムの実装方法に基づいた履歴
+# print(group1_l)
+# print(group2_l)
+
+# 「実装時に頭真っ白…」でもう悩まない！プログラムの実装方法に基づいた履歴----------------------------------------------------
 
 # 2つの空のリストを作成する
 # group1_l = []
