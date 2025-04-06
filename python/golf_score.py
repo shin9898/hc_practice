31d91e09320c22a193017f211b3e74fe48cde51d
koki <shintech9898@gmail.com>
2025-04-06 13:43:14 +0900

    Create a score difference calculation function

--- python/golf_score.py
+++ python/golf_score.py
@@ -1,22 +1,20 @@
-import sys
-
 # 値受け取る関数
+def take_values_func():
 # case_1.txtの値を受け取りX(規定打数)とY(プレイヤー打数)に代入
-input_line = int(input())
-for X in range(input_line):
-    Y = input().rstrip().split(' ')
-    print(X)
-    print(Y)
-    # return X, Y
+  x_list = list(map(int, input().split()))
+  y_list = list(map(int, input().split()))
+  return x_list, y_list
 
 # スコア差計算関数
-# def scoring(X, Y):
+def scoring_func(x_list, y_list):
 
 # X[i] - Y[1]で規定打数とプレイヤー打数のresult(差)を計算
-    # for x, y in zip(X, Y):
-    #   result = x - y
-    #   print(result)
-    #   return score_output(X, Y, result)
+    result_list = []
+    for x, y in zip(x_list, y_list):
+      result = x - y
+      result_list.append(result)
+      # print(result)
+    return result_list
 
 # スコア出力関数
 # def score_output(X, Y, result):
@@ -36,3 +34,11 @@ for X in range(input_line):
 # result = 1 : ボギー
 
 # result >= 2 : f"{result}ボギー
+
+# 関数実行
+if __name__ == '__main__':
+    x_list, y_list = take_values_func()
+    # print(x_l)
+    # print(y_l)
+    result = scoring_func(x_list, y_list)
+    # print(result)
